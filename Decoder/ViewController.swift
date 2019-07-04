0a346d426202db9afe83cd11ec5ab5cadf62f1ab
Alivelu <alivelu7193@gmail.com>
2019-07-04 12:23:28 +0530

    first commit

--- Decoder/ViewController.swift
+++ Decoder/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var apps=[allApps]()
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+  
+      @IBAction func btn(_ sender:Any)
+      {
+ let stbd=UIStoryboard(name: "Main", bundle: nil)
+        
+    let nvc=stbd.instantiateViewController(withIdentifier: "TopFreeVC") as! TopFreeVC
+        self.navigationController?.pushViewController(nvc, animated: true)
+            
+            let url = URL(string: "https://rss.itunes.apple.com/api/v1/in/ios-apps/top-free/all/10/explicit.json")
+            
+            URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                if error==nil
+              {
+                    do{
+                        self.apps = try JSONDecoder().decode([allApps].self, from:data!)
+                        DispatchQueue.main.sync {
+                            print(data!)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+                        }
+                        print(data)
+                    }catch{
+
+                        print("json error")
+                    }
+                    
+                }
+            }
+    .resume()
     }
-
-
 }
 
---
+++ allApps.swift
@@ -0,0 +1,17 @@
+//
+//  allApps.swift
+//  Decoder
+//
+//  Created by Alivelu Ravula on 4/24/18.
+//  Copyright © 2018 brn. All rights reserved.
+//
+
+import Foundation
+
+struct  allApps:Decodable
+{
+    let name:String
+    let artworkUrl100:String
+    
+  
+}
